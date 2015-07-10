dcba91c0c1c9aa3daee75a99047a6ff4a4d45f3d
Noah Petherbridge <root@kirsle.net>
2015-07-09 22:46:51 -0700

    Migrate config system to YamlSettings

--- rophako/settings.py
+++ rophako/settings.py
@@ -5,58 +5,49 @@ import os
 import datetime
 from attrdict import AttrDict
 from ConfigParser import ConfigParser
+from yamlsettings import YamlSettings
 
 from rophako.plugin import load_plugin
 
 # Get the base directory of the git root.
 basedir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir))
 
-# https://github.com/bcj/AttrDict/issues/20
-if not hasattr(AttrDict, "copy"):
-    setattr(AttrDict, "copy", lambda self: self._mapping.copy())
-
 class ConfigHandler(object):
     settings = None
 
     def load_settings(self):
-        """Load the settings files and make them available in the global config."""
-        self.settings = ConfigParser(dict_type=AttrDict)
+        """Load the settings and make them available in the global config."""
+        settings_file = os.environ.get("ROPHAKO_SETTINGS", "settings.yml")
+        project_settings = YamlSettings("defaults.yml", settings_file,
+            default_section="rophako")
+        self.settings = project_settings.get_settings()
 
-        # Set dynamic default variables.
-        self.settings.set("DEFAULT", "_basedir", basedir)
-        self.settings.set("DEFAULT", "_year", str(datetime.datetime.now().strftime("%Y")))
-
-        # Read the defaults and then apply the custom settings on top.
-        settings_file = os.environ.get("ROPHAKO_SETTINGS", "settings.ini")
-        self.settings.read(["defaults.ini", settings_file])
+        # Extrapolate {basedir} in certain keys.
+        # TODO: find a better way...
+        self.site.site_root = self.site.site_root.format(basedir=basedir)
+        self.emoticons.root_private = self.emoticons.root_private.format(
+            basedir=basedir
+        )
+        self.photo.root_private = self.photo.root_private.format(basedir=basedir)
 
     def print_settings(self):
-        """Pretty-print the contents of the configuration as JSON."""
-        for section in self.settings.sections():
-            print "[{}]".format(section)
-            for opt in self.settings.options(section):
-                print "{} = {}".format(opt, repr(self.settings.get(section, opt)))
-            print ""
+        """Pretty-print the contents of the configuration."""
+        print self.settings
 
     def load_plugins(self):
         """Load all the plugins specified by the config file."""
-        for plugin in self.plugins.blueprints.split("\n"):
+        for plugin in self.blueprints:
             plugin = plugin.strip()
-            if not plugin:
-                continue
+            if not plugin: continue
             load_plugin(plugin)
-        for custom in self.plugins.custom.split("\n"):
+        for custom in self.custom:
             custom = custom.strip()
-            if not custom:
-                continue
+            if not custom: continue
             load_plugin(custom, as_blueprint=False)
 
     def __getattr__(self, section):
-        """Attribute access for the config object.
-
-        You can access config settings via Config.<section>.<name>, for example
-        Config.site.notify_email and Config.blog.posts_per_page. All results are
-        returned as strings per ConfigParser, so cast them if you need to."""
-        return AttrDict(dict(self.settings.items(section)))
+        """Attribute accessor for the config object. Acts as a simple pass-thru
+        to YamlSettings."""
+        return getattr(self.settings, section)
 
 Config = ConfigHandler()
